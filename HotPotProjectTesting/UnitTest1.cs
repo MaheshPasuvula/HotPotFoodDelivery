9ca9fb477d7e84d75c32f764e1da61d5404536da
MaheshPasuvula <maheshnlvp@gmail.com>
2024-04-30 19:39:46 +0530

    final code

---
+++ HotPotProjectTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HotPotProjectTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
